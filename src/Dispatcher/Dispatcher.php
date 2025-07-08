8f121a7eaf4d8d2802fcf160ff2032b3380bb7d3
goldisbest <bachalex86@gmail.com>
2025-07-08 14:19:31 +0200

    removed xmlreader code and simplified some expressions

--- src/Dispatcher/Dispatcher.php
+++ src/Dispatcher/Dispatcher.php
@@ -81,13 +81,11 @@ class Dispatcher extends AbstractModuleDispatcher implements HelperFactoryAwareI
 
         $data['rssrtl'] = $data['params']->get('rssrtl', 0);
 
-        // $feedHelper = $this->getHelperFactory()->getHelper('FeedHelper');
         $feeds = [];
 
         $nodes = $data['urls'];
         $master = curl_multi_init();
         $node_count = count($nodes);
-        // $node_count = 2;
         $curl_arr = [];
         $master = curl_multi_init();
 
@@ -113,27 +111,18 @@ class Dispatcher extends AbstractModuleDispatcher implements HelperFactoryAwareI
         for ($i = 0; $i < $node_count; $i++) {
             $results[$i] = curl_multi_getcontent($curl_arr[$i]);
         }
-        // echo $results[0];
 
         $reader = new \XMLReader();
         for ($x = 0; $x < count($nodes); $x++) {
-        // for ($x = 0; $x < 10; $x++) {
 
-            // $feeds[$x] = $feedHelper->getFeedInformation($data['params'], $data['urls'][$x]);
             $feed = new MyFeed();
             $feed->feedUri = $nodes[$x];
-            // $reader->open($data['urls'][$x]);
 
-            // TODO: suppress errors? , null, LIBXML_NOERROR | LIBXML_ERR_NONE | LIBXML_NOWARNING)
-            // $reader->XML($results[$x]);
-            // $reader->XML($results[$x], null, LIBXML_NOERROR | LIBXML_ERR_NONE | LIBXML_NOWARNING);
-
-            $simpleXML = new \SimpleXMLElement(str_replace(['media:thumbnail', 'content:encoded'], ['mediathumbnail', 'contentencoded'], $results[$x]));
-            if ($simpleXML->getName() == 'rss') {
-                $rssFeed = $simpleXML->channel;
-            } else if ($simpleXML->getName() == 'feed') {
-                $rssFeed = $simpleXML;
-            }
+            $simpleXML = new \SimpleXMLElement($results[$x]);
+            $rssFeed = match ($simpleXML->getName()) {
+                'rss' => $simpleXML->channel,
+                'feed' => $simpleXML,
+            };
 
             $item = $this->first_tag_match($rssFeed, ['entry', 'item']);
 
@@ -142,60 +131,17 @@ class Dispatcher extends AbstractModuleDispatcher implements HelperFactoryAwareI
             $feed->pubDate = $this->first_tag_match($item, ['pubDate', 'published']);
 
             // Order is important here. Some blogs have content encoded and description. We want content encoded if available.
-            $feed->description = $this->first_tag_match($item, ['contentencoded', 'description', 'summary', 'content']);
+            $contentEncoded = $item->children('content', TRUE)->encoded;
+            $feed->description = $contentEncoded ?: $this->first_tag_match($item, ['description', 'summary', 'content']);
 
-            $feed->imgUri = $item->mediathumbnail['url'] ?? '';
+            $thumbnail = $item->children('media', TRUE)->thumbnail;
+            $feed->imgUri = $thumbnail ? $thumbnail->attributes()->url : $this->get_image_path($feed->description);
 
-            foreach ($item->link as $link) {
-                if (!isset($link['href'])) {
-                    $feed->uri = $link;
-                    break;
-                } else if ($link['rel'] == 'alternate') {
-                    $feed->uri = $link['href'];
+            foreach ($item->link as $link) {    
+                if (!isset($link['href']) || $link['rel'] == 'alternate') {
+                    $feed->uri = $link['href'] ?: $link;
                     break;
                 }
-            }
-
-            /*
-            $firstEntryFound = false;
-            while ($reader->read()) {
-                if ($reader->nodeType == \XMLReader::ELEMENT) {
-                    // TODO: values should only be set once.
-                    $tag = $reader->name;
-                    if ($tag == 'title' && !$firstEntryFound) {
-                        $feed->title = $reader->readInnerXml();
-                    } else if ($tag == 'pubDate' && $firstEntryFound) {
-                        $feed->pubDate = $reader->readInnerXml();
-                    } else if ($tag == 'published' && $firstEntryFound) {
-                        $feed->pubDate = $reader->readInnerXml();
-                    } else if (in_array($tag, ['item', 'entry'], true) && !$firstEntryFound) {
-                        $firstEntryFound = true;
-                    } else if ($tag == 'title' && $firstEntryFound) {
-                        $feed->firstEntry = $reader->readInnerXml();
-                    }
-                    // Some blogs have description and content:encoded tags, so this only works if the order is correct
-                    else if (in_array($tag, ['description', 'summary', 'content', 'content:encoded'], true) && $firstEntryFound) {
-                        $feed->description = $reader->readString();
-                    } else if ($tag == 'link' && $firstEntryFound) {
-                        if ($reader->getAttribute('rel') === 'alternate') {
-                            $feed->uri = $reader->getAttribute('href');
-                        } else if ($reader->getAttribute('href') === null) {
-                            $feed->uri = $reader->readString();
-                        }
-                    } else if ($tag == 'media:thumbnail' && $firstEntryFound) {
-                        $feed->imgUri = $reader->getAttribute('url');
-                    }
-                    // Stop when we encounter the 2nd item in the feed.
-                    else if (in_array($tag, ['item', 'entry'], true) && $firstEntryFound) {
-                        break;
-                    }
-                }
-
-            }
-            */
-
-            if (!$feed->imgUri) {
-                $feed->imgUri = $this->get_image_path($feed->description);
             }
 
             if ($feed->is_data_complete()) {
