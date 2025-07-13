d4df04e654a1964b4daef7a4276394cfd7355476
goldisbest <bachalex86@gmail.com>
2025-07-13 23:59:46 +0200

    js finally works, added expand collapse logic, removed rtl stuff

--- media/js/show-all.js
+++ media/js/show-all.js
@@ -2,13 +2,19 @@ if (!window.Joomla) {
     throw new Error('Joomla API was not properly initialised');
 }
 
+const showMoreLabel = Joomla.Text._('MOD_BLOGROLL_SHOW_MORE');
+const showLessLabel = Joomla.Text._('MOD_BLOGROLL_SHOW_LESS');
+
+var content = document.querySelector('.mod_blogroll_showall_container');
+
 const showAll = (event) => {
-    event.target.innerHTML = "jsddjd";
+    if (content.style.display === "block") {
+        content.style.display = "none";
+        event.target.innerHTML = showMoreLabel;
+    } else {
+        content.style.display = "block";
+        event.target.innerHTML = showLessLabel;
+    }
 };
 
-document.querySelectorAll('.mod_blogroll_showall').forEach(element => {
-    element.addEventListener('click', showAll);
-});
-document.querySelectorAll('.mod_blogroll_showall').forEach(element => {
-    element.innerText += "yoloo";
-});
+document.querySelector('.mod_blogroll_showall_button').addEventListener('click', showAll);
--- tmpl/default.php
+++ tmpl/default.php
@@ -1,29 +1,14 @@
 <?php
 defined('_JEXEC') or die;
 
+use Joomla\CMS\Language\Text;
+
+Text::script('MOD_BLOGROLL_SHOW_MORE');
+Text::script('MOD_BLOGROLL_SHOW_LESS');
+
 $wa = $app->getDocument()->getWebAssetManager();
 $wa->getRegistry()->addExtensionRegistryFile('mod_blogroll');
 $wa->useScript('mod_blogroll.show-all');
-
-$lang = $app->getLanguage();
-$myrtl = $params->get('rssrtl', 0);
-$direction = ' ';
-
-$isRtl = $lang->isRtl();
-
-if ($isRtl && $myrtl == 0) {
-	$direction = ' redirect-rtl';
-} elseif ($isRtl && $myrtl == 1) {
-	$direction = ' redirect-ltr';
-} elseif ($isRtl && $myrtl == 2) {
-	$direction = ' redirect-rtl';
-} elseif ($myrtl == 0) {
-	$direction = ' redirect-ltr';
-} elseif ($myrtl == 1) {
-	$direction = ' redirect-ltr';
-} elseif ($myrtl == 2) {
-	$direction = ' redirect-rtl';
-}
 ?>
 
 <style>
@@ -45,24 +30,57 @@ if ($isRtl && $myrtl == 0) {
 		margin-top: 0px;
 		object-fit: cover;
 	}
+
+	.mod_blogroll_showall_container {
+		display: none;
+	}
+
+	.mod_blogroll_showall_button {
+		display: inline;
+		padding: 0;
+		border: 0;
+		font: inherit;
+		text-decoration: none;
+		cursor: pointer;
+		background: transparent;
+		color: currentColor;
+		float: right;
+	}
+
+	.mod_blogroll_showall_button:hover,
+	.mod_blogroll_showall_button:active {
+		color: blue;
+	}
 </style>
 
 <?php
-$itemDisplayCount = min(count($feeds), $params->get('rssitems', PHP_INT_MAX));
+$feedCount = count($feeds);
+$itemDisplayCount = min($feedCount, $params->get('rssitems', PHP_INT_MAX));
 for ($i = 0; $i < $itemDisplayCount; $i++) {
 	$feed = $feeds[$i];
-	if (!empty($feed) && is_string($feed)) {
-		echo $feed;
-		continue;
-	}
+	item_layout($feed, $params, $feedCount, $i);
+}
+?>
 
-	if ($feed == false) {
-		continue;
+<!-- Collapsible items -->
+<div class="mod_blogroll_showall_container">
+	<?php
+	for ($i = $itemDisplayCount; $i < $feedCount; $i++) {
+		$feed = $feeds[$i];
+		item_layout($feed, $params, $feedCount, $i);
 	}
 	?>
+</div>
 
-	<div style="direction: <?= $rssrtl ? 'rtl' : 'ltr'; ?>;width:100%;overflow:auto;"
-		class="text-<?= $rssrtl ? 'right' : 'left'; ?> feed">
+<!-- Button to collapse/expand -->
+<?php if ($feedCount > $itemDisplayCount) { ?>
+	<button class="mod_blogroll_showall_button" type="button"><?= Text::_('MOD_BLOGROLL_SHOW_MORE'); ?></button>
+<?php } ?>
+
+<?php
+function item_layout($feed, $params, $feedCount, $index)
+{ ?>
+	<div style="width:100%;overflow:auto;">
 
 		<!-- Feed image -->
 		<?php if ($params->get('rssimage', 1)): ?>
@@ -80,7 +98,7 @@ for ($i = 0; $i < $itemDisplayCount; $i++) {
 
 			<!-- Feed title -->
 			<?php if ($feed->feedTitle !== null && $params->get('rsstitle', 1)): ?>
-				<h6 class="<?= $direction; ?>">
+				<h6>
 					<a class="blogroll" href="<?= $feed->feedUri ?>
 							" target="_blank" rel="noopener">
 						<?= $feed->feedTitle; ?></a>
@@ -96,12 +114,9 @@ for ($i = 0; $i < $itemDisplayCount; $i++) {
 			<span style="color:#404040"> - <?= $feed->timeDifference; ?></span>
 		</div>
 
-		<!-- Divider between items -->
-		<?php if ($i < $itemDisplayCount - 1) { ?>
-			<hr>
+		<hr>
+		<!-- TODO: show last divider or not? -->
+		<?php if ($index < $feedCount - 1) { ?>
 		<?php } ?>
 	</div>
 <?php } ?>
-
-<button class="mod_blogroll_showall" type="button">"PRESS ME"</button>
-
